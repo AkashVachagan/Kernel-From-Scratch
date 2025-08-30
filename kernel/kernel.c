3f53132ab58c6700b92c0d3df94484bbefc6c370
AkashVachagan <akash.vachagan@gmail.com>
2025-08-30 14:11:51 +0530

    Initial commit

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -4,10 +4,9 @@
 void main(void){
 	clear_screen();
 	kprint("This is the first line\n");
-	kprint("Hi my name is Akash\n");
 	kprint("Testing to see if new line works\n");
 	kprint_color("This should be in color green!!!\n", GREEN);
-	kprint_color("Just checking to see if this wraps around you know, so i am gonna blabber some stuff and you are just gonna have to deal with it. i am making a lot of typos, i should learn typing, touch typing specifically\n", RED);
+	kprint_color("Just checking to see if this wraps around. This is some garbage text but it should be long enough that the text wraps to the next line\n", RED);
 	kprint_color_bg("The background should be white and the text should be black", BLACK, WHITE);
 	while(1) {}
 }
